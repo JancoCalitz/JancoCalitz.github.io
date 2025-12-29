0f628d9b8b25167cdb99ab780104f9831da2cfa3
Janco Calitz <107679842+JancoCalitz@users.noreply.github.com>
2025-12-29 11:20:25 +0200

    Update script.js
    added basic script file for the wedding

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const weddingDate = new Date("August 1, 2026 15:00:00").getTime();
+const countdown = document.getElementById("countdown");
 
+setInterval(() => {
+  const now = new Date().getTime();
+  const diff = weddingDate - now;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((diff / (1000 * 60)) % 60);
+
+  countdown.innerHTML = `
+    <strong>${days}</strong> days
+    <strong>${hours}</strong> hours
+    <strong>${minutes}</strong> minutes
+  `;
+}, 1000);
